570c312ec87b220c677f1dc621a6e153865d8bbe
Beiryu <dinhnguyenkhanh210401@gmail.com>
2025-08-30 10:18:58 +0700

    format

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@
 
 import './framer/styles.css';
 
-import { Suspense, lazy, useEffect, useState } from 'react';
-
 // import NavFramerComponent from './framer/navigation/nav';
 import HeroFramerComponent from './framer/hero';
 import PartnersFramerComponent from './framer/partners';
@@ -13,71 +11,6 @@ import AppFramerComponent from './framer/app';
 import OnboardingFramerComponent from './framer/onboarding';
 import CaseStudiesFramerComponent from './framer/case-studies';
 import CalculatorRoiFramerComponent from './framer/calculator-roi';
-
-// const LazyFooterFramerComponent = lazy(
-//   () => import('./framer/navigation/footer')
-// );
-
-// const FooterFramerComponent = {
-//   Responsive: function ResponsiveFooter() {
-//     return (
-//       <Suspense
-//         fallback={
-//           <div className="w-full h-96 bg-gray-100 animate-pulse rounded-xl" />
-//         }
-//       >
-//         <LazyFooterFramerComponent />
-//       </Suspense>
-//     );
-//   },
-// };
-
-const LazyLoadComponent = ({
-  children,
-  id,
-  height = 'h-64',
-}: {
-  children: React.ReactNode;
-  id: string;
-  height?: string;
-}) => {
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          setIsVisible(true);
-          observer.disconnect();
-        }
-      },
-      { rootMargin: '200px' }
-    );
-
-    const currentElement = document.getElementById(id);
-    if (currentElement) {
-      observer.observe(currentElement);
-    }
-
-    return () => {
-      if (currentElement) {
-        observer.disconnect();
-      }
-    };
-  }, [id]);
-
-  return (
-    <div id={id} className={`w-full ${!isVisible ? height : ''}`}>
-      {isVisible ? (
-        children
-      ) : (
-        <div
-          className={`w-full ${height} bg-gray-100 animate-pulse rounded-xl`}
-        />
-      )}
-    </div>
-  );
-};
 
 export default function App() {
   return (
